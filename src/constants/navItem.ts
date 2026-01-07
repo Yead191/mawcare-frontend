bde7300b3191d6b75d5b34d2092e569e6d65fa6a
Yead191 <yead191@gmail.com>
2026-01-07 14:49:32 +0600

    first commit

---
+++ src/constants/navItem.ts
@@ -0,0 +1,9 @@
+const navItems = [
+  { labelKey: "home", href: "/" },
+  { labelKey: "about", href: "/about" },
+  { labelKey: "contactUs", href: "/contact-us" },
+  { labelKey: "terms", href: "/terms-condition" },
+  { labelKey: "privacy", href: "/privacy-policy" },
+];
+
+export default navItems;
